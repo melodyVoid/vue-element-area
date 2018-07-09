fe6a6867b9673087ddfa83ad4e1818f5110847fe
melodyVoid <botaoxyz@gmail.com>
2018-07-09 16:44:27 +0800

    docs(all): 添加文档示例

--- example/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-import Area from '../src/index'
-Vue.use(Area)
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Area from './area/main.vue'
-
-Area.install = Vue => Vue.component(Area.name, Area)
-
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
-
-export default Area
